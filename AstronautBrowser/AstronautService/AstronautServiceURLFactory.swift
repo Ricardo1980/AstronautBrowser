d54fac63d2b8509d6963f1f0469e740e78ac777d
Ricardo Ruiz Lopez <ricardo_ruiz_lopez@yahoo.es>
2023-01-26 11:04:27 +0000

    Remove useless comment.

--- AstronautBrowser/AstronautService/AstronautServiceURLFactory.swift
+++ AstronautBrowser/AstronautService/AstronautServiceURLFactory.swift
@@ -49,38 +49,3 @@ struct AstronautServiceURLFactory {
         baseURL.appending(path: "/astronaut/\(id)")
     }
 }
-//
-//enum Filter: Int, CustomStringConvertible {
-//    case active = 1
-//    case retired = 2
-//    case inTraining = 3
-//    case lostInFlight = 4
-//    case lostInTraining = 5
-//    case diedWhileInActiveService = 6
-//    case dismissed = 7
-//    case resignedDuringTraining = 8
-//    case deceased = 11
-//
-//    var description: String {
-//        switch self {
-//        case .active:
-//            return "Active"
-//        case .retired:
-//            return "Retired"
-//        case .inTraining:
-//            return "In-Training"
-//        case .lostInFlight:
-//            return "Lost In Flight"
-//        case .lostInTraining:
-//            return "Lost In Training"
-//        case .diedWhileInActiveService:
-//            return "Died While In Active Service"
-//        case .dismissed:
-//            return "Dismissed"
-//        case .resignedDuringTraining:
-//            return "Resigned during Training"
-//        case .deceased:
-//            return "Deceased"
-//        }
-//    }
-//}
